699b366a02a5d504940de808f574715ef148f305
Sven Lehnert <mail@sven-lehnert.de>
2017-10-08 15:48:43 +0200

    first working version

--- includes/form-elements.php
+++ includes/form-elements.php
@@ -63,10 +63,32 @@ function buddyforms_rtmedia_create_frontend_form_element( $form, $form_args ) {
 			ob_start();
 
 				// Test using the shortcode
-				//echo do_shortcode('[rtmedia_uploader context="' . $post_type . '" context_id="' . $post_id . '" album_id="3"]');
+				//echo do_shortcode('[rtmedia_uploader context="' . $post_type . '" context_id="' . $post_id . '"]');
+			$user_id = get_current_user_id();
+
+
+
+			if( $post_id == 0 ){
+				$context_id = substr( md5( time() * rand() ), 0, 10 );
+			} else {
+				$context_id = get_post_meta( $post_id, '_rtmedia_temp_context_id', true );
+			}
+
+			echo '<input name="_rtmedia_temp_context_id" type="hidden" value="' . $context_id . '">';
+
+
+			echo do_shortcode('[rtmedia_uploader context="' . $post_type . '" context_id="'.$context_id.'" media_type="all" media_author="'.$user_id.'"]');
+
+
+				echo '<hr>';
+
+				echo do_shortcode('[rtmedia_gallery global="false" context="' . $post_type . '" context_id="'.$context_id.'"  media_type="all" media_author="'.$user_id.'"]');
+				
+
+
 
 				// Test using the function
-				rtmedia_uploader(array('context' => 'profile', 'context_id' => 'profile', 'album_id' => 3));
+				//echo rtmedia_uploader(array('context' => 'profile', 'context_id' => 'profile', 'album_id' => 3));
 
 			// Get the html
 			$rtmedia_uploader_html = ob_get_clean();
@@ -83,3 +105,43 @@ function buddyforms_rtmedia_create_frontend_form_element( $form, $form_args ) {
 
 // Hook the new created rtMedia form element to the form
 add_filter( 'buddyforms_create_edit_form_display_element', 'buddyforms_rtmedia_create_frontend_form_element', 1, 2 );
+
+
+/*
+ * Save ACF Fields
+ *
+ */
+function buddyforms_rtmedia_update_post_meta( $customfield, $post_id ) {
+	global $post;
+
+	if ( $customfield['type'] == 'rtmedia' ) {
+
+		if( isset( $_POST['_rtmedia_temp_context_id'] ) ){
+
+
+			update_post_meta($post_id, '_rtmedia_temp_context_id', $_POST['_rtmedia_temp_context_id'] );
+
+			$rt_attr = array(
+				'context_id' => $_POST['_rtmedia_temp_context_id'],
+				'media_author' => get_current_user_id(),
+			);
+
+			$model = new RTMediaModel();
+			$results = $model->get( $rt_attr );
+
+			if ( $results ) {
+				foreach( $results as $image ) {
+					//update_rtmedia_meta( $image->media_id, '_context_id', $post_id );
+
+
+					$media_post = wp_update_post( array(
+						'ID'            => $image->media_id,
+						'post_parent'   => $post_id,
+					), true );
+				}
+			}
+		}
+
+	}
+}
+add_action( 'buddyforms_update_post_meta', 'buddyforms_rtmedia_update_post_meta', 10, 2 );
